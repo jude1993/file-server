d58816f7d7fb8403e9af1be558eb37203343c84c
wupeng <893860859@qq.com>
2018-07-02 10:10:30 +0800

    修改包名

--- src/test/java/com/jude/fileserver/FileServerApplicationTests.java
+++ src/test/java/com/jude/fileserver/FileServerApplicationTests.java
@@ -1,8 +1,9 @@
-package com.example.fileserver;
+package com.jude.fileserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
