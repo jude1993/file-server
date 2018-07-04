a35ded3b0d4d1a00313d3900cdce0efac70d437d
wupeng <893860859@qq.com>
2018-07-04 15:00:43 +0800

    ftp

--- src/main/java/com/jude/fileserver/controller/FileController.java
+++ src/main/java/com/jude/fileserver/controller/FileController.java
@@ -3,7 +3,6 @@ package com.jude.fileserver.controller;
 import com.jude.fileserver.file.client.Document;
 import com.jude.fileserver.file.client.Utils;
 import org.apache.tomcat.util.http.fileupload.FileItem;
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
 import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -71,41 +69,5 @@ public class FileController {
                 e.printStackTrace();
             }
         }
-    }
-
-    public void doUpload(HttpServletRequest request,HttpServletResponse response) throws IOException{
-        request.setCharacterEncoding("UTF-8");
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        ServletFileUpload upload = new ServletFileUpload(factory);
-        try {
-            List items = upload.parseRequest(request);
-            Iterator itr = items.iterator();
-            while (itr.hasNext()) {
-                FileItem item = (FileItem) itr.next();
-                if (item.isFormField()) {
-                    System.out.println("表单参数名:" + item.getFieldName() + "，表单参数值:" + item.getString("UTF-8"));
-                } else {
-                    if (item.getName() != null && !item.getName().equals("")) {
-                        System.out.println("上传文件的大小:" + item.getSize());
-                        System.out.println("上传文件的类型:" + item.getContentType());
-                        // item.getName()返回上传文件在客户端的完整路径名称
-                        System.out.println("上传文件的名称:" + item.getName());
-
-                        File tempFile = new File(item.getName());
-                        File file = new File(sc.getRealPath("/") + savePath, tempFile.getName());
-                        item.write(file);
-                        request.setAttribute("upload.message", "上传文件成功！");
-                    }else{
-                        request.setAttribute("upload.message", "没有选择上传文件！");
-                    }
-                }
-            }
-        }catch(FileUploadException e){
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-            request.setAttribute("upload.message", "上传文件失败！");
-        }
-        request.getRequestDispatcher("/uploadResult.jsp").forward(request, response);
     }
 }
